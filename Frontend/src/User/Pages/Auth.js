b73fd388a3b5c26bef2b81d581bc0ab2b80e36fe
Harsh Tripathi <harshtripathistar@gmail.com>
2023-01-02 18:26:11 +0530

    new Dev Commit

--- Frontend/src/User/Pages/Auth.js
+++ Frontend/src/User/Pages/Auth.js
@@ -43,7 +43,7 @@ const Auth = () => {
     if (isLoginMode) {
       try {
         const responseData = await sendRequest(
-          "http://localhost:5000/api/users/login",
+          `${process.env.REACT_APP_BACKEND_URL}/api/users/login`,
           "POST",
           JSON.stringify({
             email: formState.inputs.email.value,
@@ -65,7 +65,7 @@ const Auth = () => {
         formData.append("image", formState.inputs.image.value);
 
         const responseData = await sendRequest(
-          "http://localhost:5000/api/users/signup",
+          `${process.env.REACT_APP_BACKEND_URL}/users/signup`,
           "POST",
           formData
         );
